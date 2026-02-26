92c291a031cd88a664254541efa8e50c66ec2a8f
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2026-02-26 12:18:32 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <>
       <header className="header">
-        <div className="logo">HER_BROWNIES</div>
+        <div className="logo">HER BROWNIES</div>
 
         <nav className={`nav ${menuOpen ? "active" : ""}`}>
           <a href="#hero">Home</a>
