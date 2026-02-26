89824ff035df555aba9877df32f63beaf892c276
Akash Babu GJ <36225867+akasgj007@users.noreply.github.com>
2026-02-26 12:46:48 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,25 @@
+import orderIcon from "../assets/order-icon.png";
+
+const phoneNumber = "919786211976"; 
+
+const message = `Hi! I would like to order 1 ${name} priced at ₹${price}.`;
+const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
 function ProductCard({ name, weight, price }) {
   return (
     <div className="product-card tilt-card">
       <h3>{name}</h3>
       <p>{weight}</p>
       <span>₹{price}</span>
+      <a
+  href={whatsappURL}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="order-btn"
+>
+  <img src={orderIcon} alt="Order" className="order-icon" />
+  Place Order
+</a>
     </div>
   );
 }
